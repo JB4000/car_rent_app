6f4a21d1cb07b34c275f5034668101c07138df74
JB4000 <jabi0002@stud.ek.dk>
2025-12-03 12:42:40 +0100

    flow for getting cars available for lease. Controller and html used for testing now removed. Added Car java object, CarStatus enum, custom CarRowMapper, CarRepository, CarService. Added mapper package for custom mappers.

--- src/main/java/com/pug/car_rent_app/controller/HomeController.java
+++ src/main/java/com/pug/car_rent_app/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.pug.car_rent_app.controller;
 
 
+import com.pug.car_rent_app.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
-    // alberts test comment
 
 
 
